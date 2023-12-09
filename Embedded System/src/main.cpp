fe79679ca393cff9eea463eea1a1aa0737b39ba3
deLini1612 <ling.npl.1612@gmail.com>
2023-12-09 19:08:27 +0700

    Upload demo code 1

---
+++ Embedded System/src/main.cpp
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+
+// put function declarations here:
+
+#include <IRremote.h>
+
+#define LED_PIN 13
+#define IR_RECEIVER_PIN 5
+
+IRrecv receiver(IR_RECEIVER_PIN);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_PIN,OUTPUT);
+  receiver.enableIRIn();
+  Serial.println("=============Welcome to our programe===========\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (receiver.decode()) {
+    Serial.print("Pushed a button: ");
+    // Serial.println(receiver.decodedIRData.command);
+    Serial.print(receiver.decodedIRData.command);
+
+    switch (receiver.decodedIRData.command)
+    {
+    case 10:  Serial.println(": INPUT");          break;
+    case 11:  Serial.println(": POWER");          break;
+    case 12:  Serial.println(": MENU");           break;
+    case 14:  Serial.println(": ESC");            break;
+    case 42:  Serial.println(": KEYSTONE +");     break;
+    case 43:  Serial.println(": D-ZOOM +");       break;
+    case 44:  Serial.println(": VOLUME +");       break;
+    case 45:  Serial.println(": KEYSTONE -");     break;
+    case 46:  Serial.println(": D-ZOOM -");       break;
+    case 47:  Serial.println(": VOLUME -");       break;
+    case 58:  Serial.println(": BLANK");          break;
+    case 59:  Serial.println(": FREEZE");         break;
+    case 60:  Serial.println(": ECO");            break;
+    case 74:  Serial.println(": UP");             break;
+    case 75:  Serial.println(": DOWN");           break;
+    case 76:  Serial.println(": ENTER");          break;
+    case 77:  Serial.println(": LEFT");           break;
+    case 78:  Serial.println(": RIGHT");          break;
+    case 90:  Serial.println(": TIMER");          break;
+    case 91:  Serial.println(": AUTO");           break;
+    case 92:  Serial.println(": ASPECT");         break;
+    case 93:  Serial.println(": FUNC");           break;
+    default:
+      Serial.println(": Receive IR Error!"); break;
+    }
+
+    digitalWrite(LED_PIN, HIGH);
+
+    switch (receiver.decodedIRData.protocol) {
+      case NEC: Serial.println("NEC"); break ;
+      case SONY: Serial.println("SONY"); break ;
+      case RC5: Serial.println("RC5"); break ;
+      case RC6: Serial.println("RC6"); break ;
+      case SHARP: Serial.println("SHARP"); break ;
+      case JVC: Serial.println("JVC"); break ;
+      case SAMSUNG: Serial.println("SAMSUNG"); break ;
+      case LG: Serial.println("LG"); break ;
+      case WHYNTER: Serial.println("WHYNTER"); break ;
+      case PANASONIC: Serial.println("PANASONIC"); break ;
+      case DENON: Serial.println("DENON"); break ;
+    default:
+      case UNKNOWN: Serial.println("UNKNOWN"); break ;
+    }
+    Serial.println("");
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
+    receiver.resume();
+  }
+}
